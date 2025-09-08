cff0a5ca720dee4ba14b1b68516792e06b655ab9
MI_Pgs <miracleintelligence@hotmail.com>
2025-09-08 16:09:16 +0300

    https://github.com/fishmarch/ORB_SLAM3_Fixed
    The original codes may invalidate the iterator because of deleting bad map points.

--- src/Map.cc
+++ src/Map.cc
@@ -358,7 +358,9 @@ namespace ORB_SLAM3
     void Map::PreSave(std::set<GeometricCamera *> &spCams)
     {
         int nMPWithoutObs = 0;
-        for (MapPoint *pMPi : mspMapPoints)
+
+        vector<MapPoint *> vMPs = GetAllMapPoints();
+        for (MapPoint *pMPi : vMPs)
         {
             if (!pMPi || pMPi->isBad())
                 continue;
